6e6af9ace796cec36c166edc496b8e55142c7f02
marcosdrey <programmarcos05@gmail.com>
2025-03-18 17:13:13 -0300

    feat: add create users endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+from fast_zero import schemas
 
 app = FastAPI()
+
+database: list[schemas.UserDB] = []
 
 
 @app.get("/")
 def read_root():
     return {"message": "Olá Mundo!"}
+
+
+@app.post(
+    "/users/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.UserPublic,
+)
+def create_user(user: schemas.UserSchema):
+    new_user = schemas.UserDB(**user.model_dump(), id=len(database) + 1)
+
+    if any(
+        user_db.model_dump()["username"] == new_user.username
+        for user_db in database
+    ):
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, detail="Username already exists"
+        )
+
+    database.append(new_user)
+    return new_user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,48 @@
 from fastapi import status
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
 
 
-def test_root_should_return_ok_and_hello_world():
-    client = TestClient(app)
-
+def test_root_should_return_ok_and_hello_world(client):
     response = client.get("/")
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_create_valid_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "supersecretpassword",
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def test_create_user_with_username_that_already_exists(client):
+    client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "supersecretpassword",
+        },
+    )
+    same_user_response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "supersecretpassword",
+        },
+    )
+    assert same_user_response.status_code == status.HTTP_409_CONFLICT
+    assert same_user_response.json() == {
+        "detail": "Username already exists"
+    }
