f7944daa934a3d5aed573110c7c6705d0ec56ad3
marcosdrey <programmarcos05@gmail.com>
2025-03-20 12:54:34 -0300

    feat: create User model and testing database

---
+++ tests/test_db.py
@@ -0,0 +1,20 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session, mock_valid_user, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(**mock_valid_user)
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(
+        select(User).where(User.username == mock_valid_user["username"])
+    )
+
+    mock_valid_user.update({"id": 1, "created_at": time})
+
+    assert asdict(user) == mock_valid_user
