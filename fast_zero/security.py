216300faf22818778d18d5e467803e8a2deb5795
marcosdrey <programmarcos05@gmail.com>
2025-03-22 11:51:45 -0300

    chore: create password hash functions

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from jwt import encode
+from pwdlib import PasswordHash
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 SECRET_KEY = "my-secret-key"
 ALGORITHM = "HS256"
+pwd_ctx = PasswordHash.recommended()
 
 
 def create_access_token(data: dict):
@@ -15,3 +17,11 @@ def create_access_token(data: dict):
     )
     to_encode.update({"exp": expire})
     return encode(to_encode, SECRET_KEY, ALGORITHM)
+
+
+def get_password_hash(password: str):
+    return pwd_ctx.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_ctx.verify(plain_password, hashed_password)
